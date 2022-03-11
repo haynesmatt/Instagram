001afe856965cd14c4fad850df8e0aa163b9b2f1
Matt <matthaynes412@gmail.com>
2022-03-10 22:11:40 -0500

    Design cameraViewController

---
+++ Instagram/cameraViewController.swift
@@ -0,0 +1,40 @@
+//
+//  cameraViewController.swift
+//  Instagram
+//
+//  Created by Matt Haynes on 3/10/22.
+//
+
+import UIKit
+
+class cameraViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSumbitButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
