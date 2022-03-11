774eaf4ce546f3e8bebc684b7a127a83425663f0
Matt <matthaynes412@gmail.com>
2022-03-11 00:27:21 -0500

    Configure feedViewController

---
+++ Instagram/postCell.swift
@@ -0,0 +1,28 @@
+//
+//  postCell.swift
+//  Instagram
+//
+//  Created by Matt Haynes on 3/10/22.
+//
+
+import UIKit
+
+class postCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameLabel1: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
