9d82b15cc8bf24be8d1591f7d91b488e8879de11
Matt <matthaynes412@gmail.com>
2022-03-16 13:49:07 -0400

    Design commentCell

---
+++ Instagram/commentCell.swift
@@ -0,0 +1,26 @@
+//
+//  commentCell.swift
+//  Instagram
+//
+//  Created by Matt Haynes on 3/16/22.
+//
+
+import UIKit
+
+class commentCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
