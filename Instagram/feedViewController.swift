1a5911cc594e3b730db106d663382b4e7f49a754
Matt <matthaynes412@gmail.com>
2022-03-18 00:01:08 -0400

    Enable Comment Display

--- Instagram/feedViewController.swift
+++ Instagram/feedViewController.swift
@@ -36,7 +36,7 @@ class feedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground() { (posts, error) in
@@ -48,13 +48,22 @@ class feedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! postCell
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let post = posts[indexPath.row]
+        if indexPath.row == 0 {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! postCell
         
         let user = post["author"] as! PFUser
         cell.usernameLabel.text = user.username
@@ -69,7 +78,18 @@ class feedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.photoView.af_setImage(withURL: url)
         
         return cell
-        
+            
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell") as! commentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            
+            return cell
+        }
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
